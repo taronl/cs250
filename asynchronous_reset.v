56c981f7b2c766ef5f07068f489e5581346e3c02
taronl <57469042+taronl@users.noreply.github.com>
2019-11-13 18:06:08 -0800

    added test

--- asynchronous_reset.v
+++ asynchronous_reset.v
@@ -1,2 +1,29 @@
-module asynchronous_reset(
+module asynchronous_reset(  
+  input clk, reset,
+  output reg [4:0] counter
+);
   
+  always @(posedge clk or posedge reset)
+    begin
+      if(reset) counter <= 0;
+      else
+        counter <= counter + 1;
+    end
+endmodule
+
+
+
+
+/*module asynchronous_reset_test_top(
+  input clk,
+  output reg [2:0] counter
+);
+  
+  reg reset = 0;
+  
+  always @(posedge clk)
+    begin
+      asynchronous_reset uut(clk, reset, counter);
+      reset <= `reset;  
+    end
+endmodule*/
