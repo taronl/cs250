829361a4a06766468470d2577429fa7ad0091c0c
taronl <57469042+taronl@users.noreply.github.com>
2019-11-18 18:16:30 -0800

    1

--- lab1.v
+++ lab1.v
@@ -1,2 +1,13 @@
-module lab1(
-  input wire 
+module lab1(a, b, c, y);
+  input a, b, c;
+  output y;
+  
+  assign y = a & b;
+  
+endmodule 
+
+/*module lab1_top(
+  output y
+);
+  lab1 uut(1,1,y);
+endmodule
