02fa20e453d9b67c60e6e30b3dab2242a460b7d8
taronl <57469042+taronl@users.noreply.github.com>
2019-11-18 18:31:12 -0800

    lab2

--- lab2.v
+++ lab2.v
@@ -1,1 +1,16 @@
-module lab2(
+`timescale 1ns/100ps
+
+module RisingEdge_DFlipFlop(
+  input wire clk, d,
+  output reg q
+);
+  
+  reg [0:0] i;
+  
+  always @(posedge clk)
+    begin
+      q <= d + i[0];
+      i <= i + 1;
+    end
+  
+endmodule
