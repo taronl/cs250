898f04722fa84455c1cf593368d75c7d5c70b1fc
taronl <57469042+taronl@users.noreply.github.com>
2019-11-18 18:19:47 -0800

    2

--- lab1.v
+++ lab1.v
@@ -2,12 +2,12 @@ module lab1(a, b, c, y);
   input a, b, c;
   output y;
   
-  assign y = a & b;
+  assign y = (a & ~b) | (a & ~c);
   
 endmodule 
 
-/*module lab1_top(
+module lab1_top(
   output y
 );
-  lab1 uut(1,1,y);
+  lab1 uut(1,1, 1, y);
 endmodule
