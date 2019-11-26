63a55b1958d5fffcaab8027e8679d350c00b0050
taronl <57469042+taronl@users.noreply.github.com>
2019-11-25 18:38:15 -0800

    nope

--- lab4.v
+++ lab4.v
@@ -1,36 +1,15 @@
 module lab4(
-  input [7:0] in,
+  input [3:0] in,
   output out
 );
-  
   assign out = ^(in[7:0]);
 endmodule
 
 module lab4_top(
-  reg [7:0] in,
-  wire out
+  input [3:0] in,
+  output out
 );
+
+  lab4 uut(1,2,3,4,5,6,7);
   
-  lab4 uut(
-    in(in),
-    out(out);
-  
-  initial begin
-    in = 
-    
-    
-    /*in = 0;
-    #100;
-    in = 17;
-    #100;
-    in = 36;
-    #100;
-    in = 44;
-    #100;
-    in = 57;
-    #100;
-    in = 66;
-    #100;
-    in = 75;*/
-  end
- endmodule
+endmodule
