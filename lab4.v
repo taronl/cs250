1317186a5add83fd249eed6afd9a119b4204bb36
taronl <57469042+taronl@users.noreply.github.com>
2019-11-26 08:25:23 -0800

    no driver??

--- lab4.v
+++ lab4.v
@@ -1,15 +1,30 @@
-module lab4(
-  input [3:0] in,
-  output out
-);
-  assign out = ^(in[7:0]);
+module lab4(bit_in, par_out);
+  input [7:0] bit_in;
+  output par_out;
+  integer cnt = 0;
+  integer x = 0;
+  
+  always @(bit_in)
+    begin
+      for(x=0; x<8; x=x+1)
+        begin
+          if(bit_in[x] == 1)
+            cnt = cnt+1;
+        end
+      if(cnt % 2 == 0)
+        par_out = 0;
+      else
+        par_out = 1;
+    end
 endmodule
 
-module lab4_top(
-  input [3:0] in,
-  output out
-);
-
-  lab4 uut(1,2,3,4,5,6,7);
-  
+module lab_top();
+  reg [7:0] data;
+  wire parity;
+  lab4 u0 (data, parity);
+  initial
+    begin
+      data = 8'b00000000;
+      //data = 8'b10101010;
+    end
 endmodule
