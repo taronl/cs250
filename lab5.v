31d5178aee65c364368a77d8c12dc41d9a879f1b
taronl <57469042+taronl@users.noreply.github.com>
2019-12-02 07:40:37 -0800

    missing file directory?

--- lab5.v
+++ lab5.v
@@ -1,15 +1,22 @@
-module srflipflop();
+module srflipflop(clk, set, reset, q, q1);
   input wire clk, set, reset;
-  output reg [4:0] q, qbar;
-
+  output reg [2:0] q, q1;
   
-  reg [0:0] i;
+  initial
+    begin
+      q = 1'b0;
+      q1 = 1'b1;
+    end
   
   always @(posedge clk)
     begin
-      xor(q
+      case({set,reset})
+        {1'b0, 1'b0}: begin q=q; q1=q1; end
+        {1'b0, 1'b1}: begin q=1'b0; q1=1'b1; end
+        {1'b1, 1'b0}: begin q=1'b1; q1=1'b0; end
+        {1'b1, 1'b1}: begin q=1'bx; q1=1'bx; end
+      endcase
     end
-  
 endmodule
 
 /*
@@ -22,4 +29,3 @@ module srflipflop_top(
   
 endmodule
 */
-test
