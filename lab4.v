ee9a2d6d5401ee1eefa957656a3586f09d77dd72
taronl <57469042+taronl@users.noreply.github.com>
2019-12-02 17:14:43 -0800

    xor

--- lab4.v
+++ lab4.v
@@ -1,30 +1,14 @@
-module lab4(bit_in, par_out);
-  input [7:0] bit_in;
-  output par_out;
-  integer cnt = 0;
-  integer x = 0;
+module lab4(
+  input [7:0] bit_in,
+  output parity
+);
   
-  always @(bit_in)
-    begin
-      for(x=0; x<8; x=x+1)
-        begin
-          if(bit_in[x] == 1)
-            cnt = cnt+1;
-        end
-      if(cnt % 2 == 0)
-        par_out = 0;
-      else
-        par_out = 1;
-    end
+  assign
+    parity = ~^bit_in;
 endmodule
 
-module lab_top();
-  reg [7:0] data;
-  wire parity;
-  lab4 u0 (data, parity);
-  initial
-    begin
-      data = 8'b00000000;
-      //data = 8'b10101010;
-    end
-endmodule
+module lab4 uut(
+  reg [7:0] bit_in;
+  wire parity
+);
+  
