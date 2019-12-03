86251a50c49e3ce7ffd78b47447efc06c0c39485
taronl <57469042+taronl@users.noreply.github.com>
2019-12-02 17:50:57 -0800

    done

--- lab5.v
+++ lab5.v
@@ -1,6 +1,6 @@
-module srflipflop(clk, set, reset, q, q1);
-  input wire clk, set, reset;
-  output reg [2:0] q, q1;
+module srflipflop(clk, st, rst, q, q1);
+  input wire clk, st, rst;
+  output reg [0:0] q, q1;
   
   initial
     begin
@@ -10,22 +10,11 @@ module srflipflop(clk, set, reset, q, q1);
   
   always @(posedge clk)
     begin
-      case({set,reset})
-        {1'b0, 1'b0}: begin q=q; q1=q1; end
-        {1'b0, 1'b1}: begin q=1'b0; q1=1'b1; end
-        {1'b1, 1'b0}: begin q=1'b1; q1=1'b0; end
-        {1'b1, 1'b1}: begin q=1'bx; q1=1'bx; end
+      case({st,rst})
+        {1'b0, 1'b0}: begin q<=q; q1<=q1; end
+        {1'b0, 1'b1}: begin q<=1'b0; q1<=1'b1; end
+        {1'b1, 1'b0}: begin q<=1'b1; q1<=1'b0; end
+        {1'b1, 1'b1}: begin q<=1'bx; q1<=1'bx; end
       endcase
     end
 endmodule
-
-/*
-module srflipflop_top(
-  input clk,
-  output q
-);
-  
-  srflipflop uut(clk, 0, q);
-  
-endmodule
-*/
