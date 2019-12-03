882ace2bc07ee4752854f643f0bd282bafbbf9bd
taronl <57469042+taronl@users.noreply.github.com>
2019-12-02 17:58:39 -0800

    done

--- lab2.v
+++ lab2.v
@@ -2,14 +2,11 @@ module dflipflop(
   input wire clk, d,
   output reg q
 );
-  
   reg [0:0] i;
-  
   always @(posedge clk)
     begin
       q <= d;
     end
-  
 endmodule
 
 module dflipflop_top(
