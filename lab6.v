d0558396a494a876452d4ec8e3e94926624ca336
taronl <57469042+taronl@users.noreply.github.com>
2019-12-04 09:00:56 -0800

    ask opinder the problem definition

--- lab6.v
+++ lab6.v
@@ -2,18 +2,15 @@ module walksignal(
   input clk, x, y,
   output reg [0:0] sx, sy
   );
-  bit temp1, temp2; 
   always @(posedge clk)
     begin
       sx <= (x&~y);
       sy <= (~x&y);
       if((x==1)&(y==1))
         begin
-          sx <= temp1;
-          sy <= temp2;
+          sx <= x;
+          sy <= y;
         end
-      temp1 <= sx;
-      temp2 <= sy;
     end
 endmodule
 
@@ -21,5 +18,5 @@ module walksignal_top(
   input clk, x, y,
   output reg sx, sy
 );
-  walksignal uut(clk, 1, 1, sx,sy);
+  walksignal uut(clk, 0, 1, sx ,sy);
 endmodule
