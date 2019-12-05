4a0e717f4d331e3bd090fe4b5bc355b60d29c044
taronl <57469042+taronl@users.noreply.github.com>
2019-12-04 17:18:15 -0800

    done

--- lab3.v
+++ lab3.v
@@ -2,15 +2,12 @@ module fib(
   input clk,
   output reg [3:0] y
 );
-  
   reg [3:0] prev;
-  
   initial
     begin
       prev = 4'd1;
       y = 4'd0;
     end
-  
   always @(posedge clk)
     begin
       if(y >= 13)
@@ -23,6 +20,5 @@ module fib(
            y <= y + prev;
       	   prev <= y;
         end
-    end
-      
+    end  
 endmodule
