ee642cabb006b5e9012f05c55d6c849eec789716
taronl <57469042+taronl@users.noreply.github.com>
2019-12-04 17:48:03 -0800

    done

--- lab5.v
+++ lab5.v
@@ -1,20 +1,16 @@
 module srflipflop(
-  input wire clk, s, r,
-  output reg q, q1
+  input wire clk, s, r, q,
+  output reg q1
 );
-  initial
-    begin
-      q = 0;
-    end
   always @(posedge clk)
     begin
-      q1 <= ~(~(s | q) | r);
+      q1 <= ~(~(s|q)|r);
     end
 endmodule
 
 module srflipflop_top(
-  input clk, s, r,
-  output q, q1
+  input clk, s, r, q,
+  output q1
 );
-  srflipflop uut(clk, 1, 0, q, q1);
+  srflipflop uut(clk, 1, 1, 0, q1);
 endmodule
